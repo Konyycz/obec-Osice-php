084b6c00e8b5b917ae3f8173290e1728433a6919
Konyycz <pavelkonykonecny@gmail.com>
2021-03-19 17:20:50 +0100

    index fix

--- index.php
+++ index.php
@@ -8,5 +8,9 @@ if(isset($url[3])) {$url3 = $url[3];}
 
 if($url1==""){include "page_home.php";}
 elseif($url1=="dokumenty"){include "page_dokumenty.php";}
+elseif($url1=="uredni-deska" AND $url2=="archiv"){include "page_dokumenty-archiv.php";}
+elseif($url1=="uredni-deska"){include "page_uredni-deska.php";}
+elseif($url1=="uzitecne-informace"){include "page_uzitecne-informace.php";}
+elseif($url1=="akce-udalosti"){include "page_akce-udalosti.php";}
 else {include "page_home.php";}
 ?>
